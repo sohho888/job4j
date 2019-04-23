311d05f506500f6a1b0e8e1df47af03afc1ef48d
sohho888 <sohho888@gmail.com>
2019-04-23 17:43:31 +0300

    Создание package-info

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,17 @@
-package ru.job4j.calculate;
-
+/**
+* Package for calculate task.
+*
+* @author Vycheslav Ponomarev (sohho888@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
 
 public class Calculate {
-
+/**
+* Вывод Hello World
+* @param args 
+*/
+package ru.job4j.calculate;
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
