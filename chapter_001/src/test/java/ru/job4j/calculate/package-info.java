d8cf1a7b44df6d5c00d9cc7502e09f0567dc75e4
sohho888 <sohho888@gmail.com>
2019-04-25 23:05:52 +0300

    JUnit-1

---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vycheslav Ponomarev (sohho888@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
--- chapter_001/target/site/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"ru.job4j.calculate","c":"Calculate","l":"Calculate()","url":"%3Cinit%3E()"},{"p":"ru.job4j.calculate","c":"Calculate","l":"main(String[])","url":"main(java.lang.String[])"}]
