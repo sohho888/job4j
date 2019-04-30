fc2389ac634e65e429d971e08504f00c42cf33ca
sohho888 <sohho888@gmail.com>
2019-04-30 17:52:54 +0300

    Test

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,19 +1,23 @@
 package ru.job4j.calculate;
 
 /**
-* Package for calculate task.
-* @author Vycheslav Ponomarev (sohho888@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Package for calculate task.
+ * @author Vycheslav Ponomarev (sohho888@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Calculate {
-   
-/**
-* Конструктор, вывод строки в консоль.
-* @param args. 
-*/
-public static void main(String[] args) {
+
+    /**
+     * Конструктор, вывод строки в консоль.
+     * @param args.
+     */
+    public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public String echo(String input) {
+        return input;
     }
 }
