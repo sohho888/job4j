c11d9009c53daacf98982b625dbaf62f4e517678
sohho888 <sohho888@gmail.com>
2019-05-07 11:15:46 +0300

    addMaxTest-2

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,10 +5,26 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
+
+
     @Test
     public void whenMax1To2Then2() {
         Max max = new Max();
         int result = max.max(1, 2);
         assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax1To1Then1() {
+        Max max = new Max();
+        int result = max.max(2, 1);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void whenNumber1IsNumber2() {
+        Max max = new Max();
+        int result = max.max(1, 1);
+        assertThat(result, is(1));
     }
 }
