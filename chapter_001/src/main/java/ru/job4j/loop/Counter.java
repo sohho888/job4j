aede0aaf4baff3f63b4b77d70d9c697f3ad8a9c8
sohho888 <sohho888@gmail.com>
2019-05-10 23:31:52 +0300

    addCounter-2

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Counter {
     public int add(int start, int finish){
         int sum = 0;
-        for (int i=1; i<11; i++){
+        for (int i = 1; i < 11; i++){
             if(i % 2 == 0){
-                sum = sum + i;
+                sum = start + finish;
             }
         }
         return sum;
