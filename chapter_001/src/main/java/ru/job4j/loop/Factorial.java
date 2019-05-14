ac4e9de3be94f158fb4ab2aca682bd889b2fb8d5
sohho888 <sohho888@gmail.com>
2019-05-14 19:54:52 +0300

    addFactorial-2

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,11 +3,11 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int result = 0;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-            if (n ==0 || n==1){
+        for (int i = 0; i >= n; i++) {
+            if (n == 0 || n == 1){
                 return 1;
             }
+            else  result *= i;
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,4 +1,22 @@
 package ru.job4j.loop;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        Factorial factorialNumber = new Factorial();
+            int result = factorialNumber.calc(5);
+            assertThat(result, is(120));
+        }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        Factorial factorialNumber = new Factorial();
+        int result = factorialNumber.calc(0);
+        assertThat(result, is(1));
+    }
 }
