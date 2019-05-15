9008421519972462a85765f0598fb8b625312fc2
sohho888 <sohho888@gmail.com>
2019-05-15 12:52:36 +0300

    addBoard

---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ *
+ * @author Vyacheslav Ponomarev (sohho888@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BoardTest {
+
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+}
