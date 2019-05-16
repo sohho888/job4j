49dd667d7b5957c9951178e04856db4fb45335cc
sohho888 <sohho888@gmail.com>
2019-05-16 20:19:00 +0300

    addPaintTest

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -32,7 +32,24 @@ public class PaintTest {
     @Test
     public void whenPyramid4Left() {
         Paint paint = new Paint();
-        String rst = paint.pyramid(6);
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void when4Pyramid() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
         System.out.println(rst);
         assertThat(rst,
                 is(
